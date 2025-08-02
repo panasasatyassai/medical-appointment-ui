e90fea1281243948ec4b38323a8750f7212e2371
Panasa Satya Sai <satyasaipanasa@gmail.com>
2025-08-02 21:14:36 +0530

    initial2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DoctorProfile from './pages/DoctorProfile';
-import NotFound from './pages/NotFound';
-//import SearchBar from './components/SearchBar';
+import NotFound from './pages/NotFound'; 
 import ThankYou from './components/ThankYou';
 import Header from './components/Header';
 import BookedUsers  from './components/BookedUsers';
+
 import "./App.css"
 
 const App = () => (
